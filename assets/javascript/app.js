33fbc968e3d7f062be172e177735292299342ef5
unknown <ed.beaudet@gmail.com>
2018-12-07 13:08:08 -0500

    display question, timer start and stop complete

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,4 +1,8 @@
-$(document).ready(function () {
+$(document).ready()
+
+{
+
+    //Game that will have 10 questions with 4 answers each, an image will be displayed after each question is answered 
     var questions = [
 
         {
@@ -92,13 +96,60 @@ $(document).ready(function () {
         },
 
         {
-        question: "What is the name of the first Simpsons episode that aired in the U.S.",
-        answers: ["Bart the Genius",
-            "Simpsons Roasting On An Open Fire",
-            "Some Enchanted Evening",
-            "El Barto"],
-        correctAnswer: 1,
-        image: "assets/images/og.gif"
+            question: "What is the name of the first Simpsons episode that aired in the U.S.",
+            answers: ["Bart the Genius",
+                "Simpsons Roasting On An Open Fire",
+                "Some Enchanted Evening",
+                "El Barto"],
+            correctAnswer: 1,
+            image: "assets/images/og.gif"
+        }]
+
+
+
+    var themeMusic = document.createElement("audio");
+    var doh = document.createElement("audio");
+    var woohoo = document.createElement("audio");
+    var correct;
+    var wrong;
+    var answerChosen;
+    var countdown = 30;
+    var missed;
+
+    themeMusic.setAttribute("src", "assets/theme.mp3");
+    doh.setAttribute("src", "assets/Doh.mp3");
+    woohoo.setAttribute("src", "assets/woohoo.mp3");
+    $("#start").on("click", function () {
+        themeMusic.play();
+        startTime();
+        displayQuestion();
+    });
+
+    function displayQuestion() {
+        for (j = 0; j < questions.length; j++) {
+            $(".question").text(this.questions[j].question);
+            console.log(j);
+        }
     }
-    ]
-});
+
+    function startTime() {
+        intervalId = setInterval(decrement, 100);
+        //$("#timeRemaining").html("<h3>Time remaining: " + countdown + "</h3>");
+    }
+
+    function decrement() {
+        countdown--;
+        $("#timeRemaining").html("<h3>Time remaining: " + countdown + "</h3>");
+
+        if (countdown === 0) {
+            missed++;
+            $(".answers").html("<p>Time is up! </p>");
+            doh.play();
+            stopTime();
+        }
+    }
+
+    function stopTime() {
+        clearInterval(intervalId);
+    }
+}
